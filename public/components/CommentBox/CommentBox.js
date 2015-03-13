483e0138a3ae1329018cb162e08859f5b1fde301
Jake Prather <JakeHP@Zoho.com>
2015-03-12 20:02:15 -0500

    Set default state. On render obtain data from server.

--- public/components/CommentBox/CommentBox.js
+++ public/components/CommentBox/CommentBox.js
@@ -1,17 +1,32 @@
-var data = [
-    {author: "Pete Hunt", text: "This is one comment"},
-    {author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var CommentBox = React.createClass({
+    getInitialState: function() {
+        return {data: [
+            {
+                "author": "Default Author.",
+                "text": "Default comment."
+            }
+        ]};
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     render: function() {
         return (
             <div className="commentBox">
                 Hello, world! I am a CommentBox.
-                <CommentList data={this.props.data} />
+                <CommentList data={this.state.data} />
                 <CommentForm />
             </div>
         );
     }
 });
-React.render(<CommentBox data={data} />, document.getElementById('content'));
+React.render(<CommentBox url="comments.json" />, document.getElementById('content'));
